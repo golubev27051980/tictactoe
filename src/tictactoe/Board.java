9a455fc7e60b9785fc6bd8a26b5300c885804f5c
GolubevEgor <golubev27051980@mail.ru>
2016-06-24 18:39:27 +0300

    Added some function to make desidion what is board condition

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -4,6 +4,7 @@
 package tictactoe;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -22,6 +23,12 @@ public class Board {
 	private char[] diag2;
 	private char[] winX = {'X','X','X'};
 	private char[] winO = {'O','O','O'};
+	private char[] halfX1 = {' ','X','X'};
+	private char[] halfX2 = {'X',' ','X'};
+	private char[] halfX3 = {'X','X',' '};
+	private char[] halfO1 = {' ','O','O'};
+	private char[] halfO2 = {'O',' ','O'};
+	private char[] halfO3 = {'O','O',' '};
 	private List<char[]> conditions = new ArrayList<char[]>();
 	
 	//Ordinal board constructor
@@ -30,23 +37,31 @@ public class Board {
 		for (int i = 0; i < 9; i++){
 			board[i]=' ';
 		}
+		passThrough();
 	}
 	
 	//Board constructor for debugging usage
 	Board(char[] loadedBoard){
+		// if board length is wrong
 		if (loadedBoard.length != 9){
 			for (int i = 0; i < 9; i++){
 				board[i]=' ';
 			}
+			//if board filled with wrong characters
 			for (int i = 0; i < 9; i++){
 				if((board[i]!=' ')||(board[i]!='X')||(board[i]!='O')){
 					board[i]=' ';
 				}
 			}
 		}
+		passThrough();
 	}
 	
-	private void lookThrough(){
+	/**
+	 * This command pass through all 
+	 * 
+	 */
+	private void passThrough(){
 		//now fill the lines
 		for(int i = 0; i < 3; i++){
 			line1[i]=board[i];
@@ -84,6 +99,21 @@ public class Board {
 	}
 	
 	
+	private void putSymbol(char symbol, int position){
+		if ((symbol!='X')||(symbol!='O')){
+			reqSymbol();
+		}
+		if ((position < 0)||(position>8)){
+			reqPosition();
+		}
+		board[position] = symbol;
+		passThrough();
+		rebuildCondArray();
+		whoWin();
+		halfWay();
+	}
+	
+	
 	private void rebuildCondArray(){
 		conditions.clear();
 		conditions.add(line1);
@@ -96,15 +126,76 @@ public class Board {
 		conditions.add(diag2);
 	}
 	
-	
-	
-	private void whoWin(){
-		
+	private void halfWay(){
+		Iterator<char[]> it = conditions.iterator();
+		int step = 0;
+		while(it.hasNext()){
+			char[] line = (char[]) it.next();
+			if (line.equals(halfX1)){
+				putX(step);
+			}
+			if (line.equals(halfX2)){
+				putX(step);
+			}
+			if (line.equals(halfX3)){
+				putX(step);;
+			}
+			if (line.equals(halfO1)){
+				putX(step);;
+			}
+			if (line.equals(halfO2)){
+				putX(step);;
+			}
+			if (line.equals(halfO3)){
+				putX(step);;
+			}
+			step++;
+		}
 	}
 	
 	
+	private void putX(int step){
+		switch(step){
+		case(1):
+			
+		case(2):
+			
+		case(3):
+			
+		case(4):
+			
+		case(5):
+			
+		case(6):
+			
+		case(7):
+			
+		case(8):	
+			
+		}
+	}
 	
 	
+	private void whoWin(){
+		Iterator<char[]> it = conditions.iterator();
+		while(it.hasNext()){
+			char[] line = (char[]) it.next();
+			if (line.equals(winO)){
+				winOEnd();
+			}
+			if (line.equals(winX)){
+				winXEnd();
+			}
+		}
+	}
+	
+	private void winXEnd(){
+		
+	}
+	
+	private void winOEnd(){
+		
+	}
 	
 	
 }
